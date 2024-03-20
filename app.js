8f0ee63d5cf956cdfb400cac2bc69fcfd4d4110b
Arsene Shema <arsenejb21@gmail.com>
2024-03-20 16:43:54 -0600

    sportsBets

---
+++ app.js
@@ -0,0 +1,141 @@
+
+
+const players = [
+    {
+        name: "D'Marcus Williums",
+        teamNumber: 1,
+        emoji: '🏃‍♂️',
+        skill: 10
+    },
+    {
+        name: "Tyroil Smoochie-Wallace",
+        teamNumber: 1,
+        emoji: '🤾‍♂️',
+        skill: 30
+    },
+    {
+        name: "Jackmerius Tacktheratrix",
+        teamNumber: 1,
+        emoji: '🏇',
+        skill: 88
+    },
+    {
+        name: "Javaris Jamar Javarison-Lamar",
+        teamNumber: 1,
+        emoji: '🏌️‍♀️',
+        skill: 15
+    },
+    {
+        name: "D'Pez Poopsie",
+        teamNumber: 1,
+        emoji: '🏋️‍♂️',
+        skill: 77
+    },
+    {
+        name: "D'Jasper Probincrux III",
+        teamNumber: 1,
+        emoji: '🏌️‍♂️',
+        skill: 21
+    },
+    {
+        name: "Leoz Maxwell Jilliumz",
+        teamNumber: 1,
+        emoji: '🤾',
+        skill: 5
+    },
+    {
+        name: "Hingle McCringleberry",
+        teamNumber: 1,
+        emoji: '🏂',
+        skill: 99
+    },
+    {
+        name: "L'Carpetron Dookmarriot",
+        teamNumber: 1,
+        emoji: '🧘‍♀️',
+        skill: 50
+    },
+    {
+        name: "Xmus Jaxon Flaxon-Waxon",
+        teamNumber: 1,
+        emoji: '🚶‍♀️',
+        skill: 2
+    },
+    {
+        name: "Saggitariutt Jefferspin",
+        teamNumber: 2,
+        emoji: '🏋️‍♀️',
+        skill: 61
+    },
+    {
+        name: "Quatro Quatro",
+        teamNumber: 2,
+        emoji: '🤺',
+        skill: 34
+    },
+    {
+        name: "X-Wing @Aliciousness",
+        teamNumber: 2,
+        emoji: '🏄',
+        skill: 71
+    },
+    {
+        name: "Bisquiteen Trisket",
+        teamNumber: 2,
+        emoji: '🧜‍♂️',
+        skill: 76
+    },
+    {
+        name: "Scoish Velociraptor Maloish",
+        teamNumber: 2,
+        emoji: '🤸',
+        skill: 47
+    },
+    {
+        name: "Donkey Teeth",
+        teamNumber: 2,
+        emoji: '⛹️‍♀️',
+        skill: 23
+    },
+    {
+        name: "T.J. A.J. R.J. Backslashinfourth V",
+        teamNumber: 2,
+        emoji: '🕴️',
+        skill: 58
+    },
+    {
+        name: "Firstname Lastname",
+        teamNumber: 2,
+        emoji: '💃',
+        skill: 99
+    },
+    {
+        name: "Dan Smith",
+        teamNumber: 2,
+        emoji: '🧍‍♂️',
+        skill: 3
+    },
+    {
+        name: "Tiger",
+        teamNumber: 2,
+        emoji: '🐅',
+        skill: 100
+    },
+]
+
+var globalGains
+
+function draftPlayers() {
+    // let draftPlayers = players.filter
+
+
+
+
+}
+
+players.forEach(players.name)
+
+
+
+
+players 
